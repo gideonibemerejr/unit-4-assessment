010ae001e02cd2c6633979df5dd163e1553bd8e0
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-12 15:59:05 -0500

    Updates

---
+++ precache-manifest.59899ba68043abe070ed7a56f38f24b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707cd8bd62f4abf32ee3183878797e95",
+    "url": "/unit-4-assessment/index.html"
+  },
+  {
+    "revision": "094f628a8a2ad33d2c8c",
+    "url": "/unit-4-assessment/static/css/main.5c5159c3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/unit-4-assessment/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "094f628a8a2ad33d2c8c",
+    "url": "/unit-4-assessment/static/js/main.d18f556a.chunk.js"
+  },
+  {
+    "revision": "b354662c43c1b1a188d7",
+    "url": "/unit-4-assessment/static/js/runtime~main.e55f4147.js"
+  }
+]);
